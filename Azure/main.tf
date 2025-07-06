38de6e859883779186bae803bd04cc0852feb2e6
jacob-pack <txmod777@protonmail.com>
2025-07-06 19:21:15 -0400

    additional storage account and variable configs

--- Azure/main.tf
+++ Azure/main.tf
@@ -3,16 +3,17 @@ features{}
 }
 
 resource "azurerm_resource_group" "rg-JSP-storage-eu2" {
-  location = azurerm_resource_group.rg-jsp-storage-eu2.location
+  location = "East US"
   name = "rg-jsp-storage-eu2"
-
 }
 
-resource "azurerm_storage_account" "rg-JSP-storage-eu2" {
-  location = azurerm_resource_group.rg-jsp-storage-eu2.location
-  name = "rg-jsp-storage-eu2"
-  resource_group_name = azurerm_resource_group.rg-JSP-storage-eu2
-  account_tier = "Standard"
-  account_replication_type = "LRS"
 
+
+module "storageAccount" {
+source = "./modules/storageAccount"
+name = "rg-jsp-storage-eu2"
+account_tier = "Standard"
+resource_group_name = azurerm_resource_group.rg-JSP-storage-eu2
+location = azurerm_resource_group.rg-JSP-storage-eu2.location
+account_replication_type = "LRS"
 }
