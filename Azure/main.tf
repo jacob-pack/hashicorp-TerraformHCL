1f11a67996c3bee48a1fc101b6d2a8c4b310ce96
jacob-pack <txmod777@protonmail.com>
2025-07-08 20:22:16 -0400

    updated formatting, validated storageaccounts

--- Azure/main.tf
+++ Azure/main.tf
@@ -1,19 +1,19 @@
 provider "azurerm" {
-features{}
+  features {}
 }
 
 resource "azurerm_resource_group" "rg-JSP-storage-eu2" {
   location = "East US"
-  name = "rg-jsp-storage-eu2"
+  name     = "rg-jsp-storage-eu2"
 }
 
 
 
 module "storageAccount" {
-source = "./modules/storageAccount"
-name = "rg-jsp-storage-eu2"
-account_tier = "Standard"
-resource_group_name = azurerm_resource_group.rg-JSP-storage-eu2
-location = azurerm_resource_group.rg-JSP-storage-eu2.location
-account_replication_type = "LRS"
+  source                   = "./modules/storageAccount"
+  name                     = "rg-jsp-storage-eu2"
+  account_tier             = "Standard"
+  resource_group_name      = azurerm_resource_group.rg-JSP-storage-eu2
+  location                 = azurerm_resource_group.rg-JSP-storage-eu2.location
+  account_replication_type = "LRS"
 }
--- Azure/modules/storageAccount/variables.tf
+++ Azure/modules/storageAccount/variables.tf
@@ -1,5 +1,23 @@
-variable "name" {}
-variable "location" {}
-variable "resource_group_name" {}
-variable "account_tier" {}
-variable "account_replication_type" {}
+variable "name" {
+  type        = string
+  description = "storageAccountName"
+}
+variable "location" {
+  type        = string
+  description = "storage account location"
+}
+variable "resource_group_name" {
+  type        = string
+  description = "resource group name that holds the storage account"
+
+}
+variable "account_tier" {
+  type        = string
+  description = "storage account tier"
+
+}
+variable "account_replication_type" {
+  type        = string
+  description = "storage account replication type"
+
+}
