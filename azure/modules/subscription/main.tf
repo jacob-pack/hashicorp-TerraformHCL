381e549c10e5bb578fc8e5c88b563c42f4138b96
jacob-pack <txmod777@protonmail.com>
2025-07-13 19:48:35 -0400

    began parameterizing subscription module

--- azure/modules/subscription/main.tf
+++ azure/modules/subscription/main.tf
@@ -1,14 +1,22 @@
-resource "azapi_resource" "symbolicname" {
-  type = "Microsoft.Subscription/aliases@2024-08-01-preview"
-  name = "string"
-  parent_id = "string"
+locals {
+  resourceType = "Microsoft.Subscription/aliases@2024-08-01-preview"
+  resourceName = "Azure Personal"
+  resourceParentID = "/providers/Microsoft.Management/managementGroups/658333d4-2d0b-482d-b477-9b958bfc1dbb"
+  resourceSubscriptionTenantID = "658333d4-2d0b-482d-b477-9b958bfc1dbb"
+  resourceBillingScope = "/providers/Microsoft.Billing/billingAccounts/f4e807f8-12bb-5ba1-55de-bc53d3864819:bb0d1029-3709-4e3a-aaec-c741e3e059bb_2019-05-31"
+}
+
+resource "azapi_resource" "subscription" {
+  type = local.resourceType
+  name = local.resourceName
+  parent_id = local.resourceParentID
   body = {
     properties = {
       additionalProperties = {
-        subscriptionTenantId = "658333d4-2d0b-482d-b477-9b958bfc1dbb"
+        subscriptionTenantId = local.subscriptionTenantId
       }
-      billingScope = "string"
-      displayName = "Azure Personal"
+      billingScope = local.resourceBillingScope
+      displayName = local.resourceName
     }
   }
 }
