e7c95744f674f270f5e8ef00e13017a0f161fbfe
jacob-pack <txmod777@protonmail.com>
2025-07-19 22:53:02 -0400

    updated express route

--- azure/modules/expressRouteCircuit/main.tf
+++ azure/modules/expressRouteCircuit/main.tf
@@ -1,21 +1,5 @@
 locals {
-  allowClassicOperations         = false
-  circuitProvisioningState       = "Enabled"
-  enableDirectPortRateLimit      = false
-  globalReachEnabled             = false
-  serviceProviderBandwidthMbps   = 50
-  serviceProviderPeeringLocation = "Atlanta"
-  serviceProviderName            = "CenturyLink Cloud Connect" //now known as Lumen
-  skuName = "Standard_MeteredData"
-  skuFamily = "MeteredData"
-  skuTier = "Standard"
-  ignoreCasing = false
-  ignoreMissingProperty = true
-  location = "eastus2"
-  name = "excir-jp-eastus-001"
-  parentId = "/subscriptions/6f224d82-ff8c-45ee-a61d-7fd2f93a73be/resourceGroups/rg-jsp-network-001"
-  schemaValidationEnabled = true
-  type = "Microsoft.Network/expressRouteCircuits@2024-05-01"
+  name = "excir-${var.orgName}-${var.locationAbbreviation}-001"
 }
 
 resource "azapi_resource" "expressRouteCircuit" {
